30aca825541bdacf69658862ab14e02c8c623a35
ruioliveira02 <rpoliveira02@gmail.com>
2024-04-21 14:40:13 +0100

    wip

--- src/raft.py
+++ src/raft.py
@@ -2,11 +2,16 @@
 
 import logging
 from key_value import KVStore
-import threading
+from enum import Enum
+from time import time
 from ms import receiveAll, reply, send
 
 logging.getLogger().setLevel(logging.DEBUG)
 
+class RaftRole(Enum):
+    LEADER = 1
+    CANDIDATE = 2
+    FOLLOWER = 3
 
 class Raft():
     def __init__(self):
@@ -14,6 +19,23 @@ class Raft():
         self.node_ids = []
         self.node_count = 0
         self.kv_store = KVStore()
+        self.role = RaftRole.FOLLOWER
+
+        #TODO: Make persistent
+        self.currentTerm = 0
+        self.votedFor = None
+        self.log = []
+
+        self.commitIndex = 0
+        self.lastApplied = 0
+
+        # Implementation only
+        self.votedForMe = set()
+        self.electionTimer = time()
+
+        # Leader only
+        self.nextIndex = []
+        self.matchIndex = []
 
     def init(self, msg: any):
         self.node_id = msg.body.node_id
@@ -45,6 +67,81 @@ class Raft():
         else:
             reply(msg, type='cas_ok')
 
+    def check_term(self, term: int):
+        if(term > self.currentTerm):
+            self.currentTerm = term
+            self.change_role(RaftRole.FOLLOWER)
+
+    def change_role(self, role: RaftRole):
+        match role:
+            case RaftRole.LEADER:
+                self.start_heartbeats()
+                self.cancel_timeout_check()
+            case RaftRole.CANDIDATE:
+                self.start_election()
+                self.start_timeout_check()
+                self.cancel_heartbeats()
+            case RaftRole.FOLLOWER:
+                self.start_timeout_check()
+                self.cancel_heartbeats()
+
+    def start_heartbeats(self):
+        pass
+
+    def cancel_heartbeats(self):
+        pass
+
+    def start_timeout_check(self):
+        pass
+
+    def cancel_timeout_check(self):
+        pass
+
+    def start_election(self):
+        self.currentTerm += 1
+        self.votedFor = self.node_id
+
+        self.votedForMe = set()
+        self.votedForMe.add(self.node_id)
+
+        self.electionTimer = time()
+
+        # Broadcast RequestVoteRPC
+        for node in self.node_ids:
+            if node != self.node_id:
+                send(self.node_id, node, type='RequestVoteRPC', term=self.currentTerm, \
+                    candidateId=self.node_id, lastLogIndex = len(self.log), lastLogTerm = self.log[-1].term)
+
+    def more_up_to_date(self, lastLogIndex : int, lastLogTerm : int) -> bool:
+        if self.currentTerm < lastLogTerm:
+            return True
+
+        if self.currentTerm > lastLogTerm:
+            return False
+
+        return len(self.log) <= lastLogIndex
+
+    def append_entries(self, msg: any):
+        pass
+
+    def append_entries_reply(self, msg: any):
+        pass
+
+    def request_vote(self, msg: any):
+        self.check_term(msg.body.term)
+
+        if msg.body.term < self.currentTerm:
+            reply(msg, type='RequestVoteRPCReply', term=self.currentTerm, voteGranted = False)
+
+        voteGranted = (self.votedFor is None or self.votedFor == msg.body.candidateId) and \
+            self.more_up_to_date(msg.body.lastLogIndex, msg.body.lastLogTerm)
+
+        reply(msg, type='RequestVoteRPCReply', term=self.currentTerm, voteGranted = voteGranted)
+
+    def request_vote_reply(self, msg: any):
+        pass
+
+
 raft = Raft()
 
 for msg in receiveAll():
@@ -57,5 +154,13 @@ for msg in receiveAll():
             raft.write(msg)
         case 'cas':
             raft.cas(msg)
+        case 'AppendEntriesRPC':
+            raft.append_entries(msg)
+        case 'AppendEntriesRPCReply':
+            raft.append_entries_reply(msg)
+        case 'RequestVoteRPC':
+            raft.request_vote(msg)
+        case 'RequestVoteRPCReply':
+            raft.request_vote_reply(msg)
         case _:
             logging.warning('unknown message type %s', msg.body.type)
