280334c9d50ffb0b2f013cc99217de013a3b6950
ruioliveira02 <rpoliveira02@gmail.com>
2024-05-04 14:13:07 +0100

    chore: documentation

--- src/log.py
+++ src/log.py
@@ -1,8 +1,39 @@
+"""A module defining a class representing log entries of the Raft Algorithm.
+
+Raft uses logs to replicate the changes to the replicated state machines to
+followers. A log corresponds to an operation requested by a client.
+
+Typical usage example:
+
+  log1 = Log(None, 1, 1)
+  log2 = Log(msg, 2, 2)
+"""
+
 from types import SimpleNamespace
 from ms import Message
 
 class Log:
-    def __init__(self, message: Message | None, term: int, id: int):
+    """A class representing log entries of the Raft Algorithm.
+
+    Raft uses logs to replicate the changes to the replicated state machines to
+    followers. A log corresponds to an operation requested by a client.
+
+    Attributes:
+        term: The term the log was generated in
+        id:   The id of the log. Corresponds to its index in the
+        log array
+        data: A dictionary containing the information specifying
+        the log
+    """
+    def __init__(self, message: Message | None, term: int, id: int) -> None:
+        """Initializes a log entry from a Maelstrom message, current term and
+        id.
+
+        Args:
+          message: The message to parse into a log.
+          term:    The current term.
+          id:      The id of this log.
+        """
         self.term = term
         self.id = id
         self.data = {}
@@ -11,11 +42,28 @@ class Log:
 
     @staticmethod
     def from_namespace(namespace: SimpleNamespace) -> "Log":
+        """Initializes a log entry from a SimpleNamespace.
+
+        This is used as Maelstrom, when sending messages, parses classes to SimpleNamespaces.
+        As such, to garantee the type consistency of the logs, a method to parse a
+        SimpleNamespace to a Log had to be implemented.
+
+        Args:
+          namespace: The namespace to parse
+
+        Returns:
+          The log corresponding to the namespace
+        """
         log = Log(None, namespace.term, namespace.id)
         log.data = vars(namespace.data)
         return log
 
-    def __process_message__(self, message: Message):
+    def __process_message__(self, message: Message) -> None:
+        """Initializes the data dictionary based on the content of the given message.
+
+        Args:
+          message: The message to parse.
+        """
         self.data["type"] = message.body.type
 
         match message.body.type:
@@ -28,10 +76,31 @@ class Log:
                 self.data["to"] = message.body.to
 
     def __eq__(self, other: "Log") -> bool:
+        """Checks for equality against another log.
+
+        Two logs are said to be equal if their term, id, and data content are equal.
+        
+
+        Args:
+          other: The other to check for equality against.
+
+        Returns:
+          A bool indicating if the logs are equal
+        """
         return type(self) == type(other) and self.data == other.data and self.term == other.term and self.id == other.id
 
     def __str__(self) -> str:
+        """Converts a log to a string for debug purposes.
+
+        Returns
+           A string representation of the log
+        """
         return str(vars(self))
 
     def __repr__(self) -> str:
+        """Converts a log to a string for debug purposes.
+
+        Returns
+           A string representation of the log
+        """
         return str(self)
